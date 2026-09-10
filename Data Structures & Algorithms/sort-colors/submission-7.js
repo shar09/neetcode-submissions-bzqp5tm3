b4e4c7fda87ea9d18028bd2af394f0b5597bda15
shar09 <sharathtelu9@gmail.com>
2026-09-10 06:21:34 -0500

    Add: sort-colors - submission-7

---
+++ Data Structures & Algorithms/sort-colors/submission-7.js
@@ -0,0 +1,51 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {void} Do not return anything, modify nums in-place instead.
+     */
+    sortColors(nums) {
+        let i = 0, j = 0;
+        
+        while (i < nums.length) {
+            if (nums[i] === 0) {
+                [nums[i], nums[j]] = [nums[j], nums[i]];
+                j++;
+            }
+            i++;
+        }
+
+        let k = nums.length - 1, l = nums.length - 1;
+        while (k >= j) {
+            if (nums[k] === 2) {
+                [nums[k], nums[l]] = [nums[l], nums[k]];
+                l--;
+            }
+
+            k--;
+        }
+
+        return nums;
+    }
+}
+
+// 2 pointers:
+
+// 2 pass:
+
+// In the first pass:
+    // i = 0, j = 0
+    // if i === 1
+        // swap
+        // j++;
+    // else
+        // continue;
+
+// In second pass:
+    // i = length - 1 = swapIndex
+    // while i > j
+    // if j === 2
+        // swap
+        // swap index++
+        // i--;
+    // else
+        // continue
